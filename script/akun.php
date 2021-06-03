a53f73bd9ae64c180d3630e7be3a8385cac619f7
IndraYS000 <79950695+IndraYS000@users.noreply.github.com>
2021-06-03 20:04:14 +0700

    Add files via upload

---
+++ script/akun.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en">
+<style>
+
+body {
+  min-height: 1100px;
+}
+
+.headline4{
+    color: black;
+		font-family: algerian;
+		font-size: 40px;
+		text-align: Center;
+  }
+
+.card-title{
+  font-weight: bold;
+}
+
+.harga{
+  font-weight: bold;
+}
+
+</style>
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+
+    <title>Akun</title>
+
+	<Link rel="icon" href="Logo_Dummy_Barber.png" sizes="32x32">
+	<Link rel="icon" href="Logo_Dummy_Barber.png" sizes="192x192">
+	
+	
+    <title>Akun</title>
+
+  </head>
+ <body>
+<?php
+  if (isset($_GET['pesan'])){
+    if ($_GET['pesan'] == "sukses"){
+      function function_alert() {
+        echo "<script>alert('selamat datang!');</script>";
+      }
+      function_alert();
+    }
+  }
+?>
+  <!--Start Navbar-->
+
+  <nav class="navbar navbar-expand-lg navbar-dark shadow p-2" style="background-color: #000000">
+    <div class="container">
+        <a class="nav-link active" id="logo" aria-current="page" href="#"><img src="Logo_Dummy_Barber.png" height="50px"></a>
+      <a class="navbar-brand" href="#">WEBHAIRSTYLE</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">WHSPay</a>
+          </li>
+	  <li class="nav-item">
+            <a class="nav-link" href="wares.php">Our Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact-us">Kontak Kami</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pengaturan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Log out</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+    <!--End Navbar-->
+
+    <!-- Login -->
+
+    <div>
+
+<form class="form" method="post" name="login">
+    <div class="imgcontainer" style="padding-top: 2cm;padding-bottom: 1cm;">
+     <center>
+      <img src="Logo_Dummy_Barber.png" width="150" height="150" alt="Avatar">
+     </center>
+    </div>
+  </form>
+
+</div>
+
+    <!-- End Login -->
+
+<br>
+<br>
+<br>
+
+
+   <!-- Layanan Kami - Jumbotron -->
+
+
+   <section class="jumbotron text-center">
+    <h1 class="headline4">Layanan Barber Kami</h1>
+    <br>
+    <br>
+    <div class="row justify-content-center">
+    <div class="card ms-4" style="width: 12rem;">
+      <img src="Men's Hair.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"> Men's Haircut</h5>
+        <p class="card-text"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        </p>
+        <p class="harga"> Rp.40.000 </p>
+        <a href="#" class="btn btn-primary">Order</a>
+      </div>
+    </div>
+    <div class="card ms-4" style="width: 12rem;">
+      <img src="Beard Trim.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Beard Trim</h5>
+        <p class="card-text"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        </p>
+        <p class="harga"> Rp.10.000 </p>
+        <a href="#" class="btn btn-primary">Order</a>
+      </div>
+    </div>
+    <div class="card ms-4" style="width: 12rem;">
+      <img src="Treatment.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Treatment</h5>
+        <p class="card-text"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star-half fa-lg"></i>
+        </p>
+        <p class="harga"> Rp.60.000 </p>
+        <a href="#" class="btn btn-primary">Order</a>
+      </div>
+    </div>
+    <div class="card ms-4" style="width: 12rem;">
+      <img src="Wash.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Wash</h5>
+        <p class="card-text"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        <i class="fa fa-star fa-lg"></i>
+        </p>
+        <p class="harga"> Rp.30.000 </p>
+        <a href="#" class="btn btn-primary">Order</a>
+      </div>
+    </div>
+  </div>
+  </section>
+
+
+   <!-- End Layanan Kami - Jumbotron -->
+
+
+
+   <!-- Layanan Kami - Horizontal -->
+
+      <!--
+
+   <div class="card mb-3" style="max-width: 540px;">
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <img src="Men's Hair.png" alt="100">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Men's Hair</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+ -->
+
+
+
+
+
+   <!-- End Layanan Kami - Jumbotron -->
+
+        
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+  </body>
+</html>
